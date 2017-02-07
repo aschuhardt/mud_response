79bf7b8b27f1dd33eb7b571f6fd655875ded3807
Addison Schuhardt <a@schuhardt.net>
2017-02-06 22:13:16 -0800

    added an 8-bit integer code to responses to signify their type

--- src/response.rs
+++ src/response.rs
@@ -4,12 +4,18 @@ use std::net;
 use rustc_serialize::json;
 use std::io::Write;
 
+pub const CODE_TOKEN_ACKNOWLEDGE: u8 = 0;
+pub const CODE_CONNECT_ACCEPT: u8 = 1;
+pub const CODE_CONNECT_REJECT: u8 = 2;
+pub const CODE_DISCONNECT_ACK: u8 = 3;
+
 #[derive(Clone)]
 pub struct Response {
-    addr: net::SocketAddr,
-    numerical_data: Vec<f64>,
-    textual_data: String,
-    object_data: Vec<u8>,
+    pub code: u8,
+    pub addr: net::SocketAddr,
+    pub numerical_data: Vec<f64>,
+    pub textual_data: String,
+    pub object_data: Vec<u8>,
 }
 
 impl Response {
@@ -17,6 +23,7 @@ impl Response {
         match net::TcpStream::connect(self.addr) {
             Ok(mut stream) => {
                 let serializable = SerializableResponse {
+                    code: self.code,
                     num: self.numerical_data.clone(),
                     text: self.textual_data.clone(),
                     obj: self.object_data.clone(),
@@ -32,6 +39,7 @@ impl Response {
 
 #[derive(RustcDecodable, RustcEncodable)]
 struct SerializableResponse {
+    code: u8,
     num:  Vec<f64>,               //floating-point numerical data
     text: String,                 //text data
     obj: Vec<u8>,                 //serialized encoded object data
